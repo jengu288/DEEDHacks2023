edfa3155857a27cb8cb2f7465de32005598ae117
nktac <nikita.cutinho@yahoo.com>
2023-05-13 16:58:22 -0700

    Yummy caaake, thanks Aliyah! It truly is party time :D

--- app/src/main/java/personal/deedhacks2023/NikitaActivity.java
+++ app/src/main/java/personal/deedhacks2023/NikitaActivity.java
@@ -5,15 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class NikitaActivity extends AppCompatActivity {
-
+    int FEEDcount = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FEEDcount = 0;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nikita);
+        Button btn=findViewById(R.id.button);
+        ImageView slice1=findViewById(R.id.imageView3);
+        ImageView slice2=findViewById(R.id.imageView4);
+        ImageView slice3=findViewById(R.id.imageView5);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                if (FEEDcount == 0){
+                    slice1.setVisibility(View.INVISIBLE);
+                }
+                else if (FEEDcount == 1){
+                    slice2.setVisibility(View.INVISIBLE);
+                }
+                else if (FEEDcount == 2) {
+                    slice3.setVisibility(View.INVISIBLE);
+                }
+                FEEDcount++;
+            }
+        });
     }
-
     public static Intent makeLaunchIntent(Context context) {
         Intent intent = new Intent(context, NikitaActivity.class);
 
